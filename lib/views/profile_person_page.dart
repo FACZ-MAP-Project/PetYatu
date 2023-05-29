238b1f2ec1506cfee3e9d4ca8d3869ae96a74773
Haziq Zikry <haziqzikry@gmail.com>
2023-05-29 13:05:53 +0800

    edited profile page ui

--- lib/views/profile_person_page.dart
+++ lib/views/profile_person_page.dart
@@ -1,28 +1,93 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({
-    super.key,
-  });
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              radius: 50.0,
-              backgroundImage: NetworkImage(
-                  'https://www.w3schools.com/w3images/avatar2.png'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                    'https://www.w3schools.com/w3images/avatar2.png',
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                _buildProfileCard(),
+                const SizedBox(height: 20.0),
+                _buildBioCard(),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditProfilePage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Edit Profile'),
+                ),
+                const SizedBox(height: 10.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: const Text('Logout'),
+                ),
+              ],
             ),
-            const SizedBox(height: 10.0),
-            const Text(
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'lib/assets/images/logo.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+          ),
+          const Text(
+            'PetYatu',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileCard() {
+    return Card(
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
               'John Doe',
               style: TextStyle(
-                fontSize: 22.0,
+                fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
@@ -44,7 +109,7 @@ class Profile extends StatelessWidget {
             ),
             const SizedBox(height: 10.0),
             Text(
-              'Loocation: Klang, Selangor, Maalysia',
+              'Location: Klang, Selangor, Malaysia',
               style: TextStyle(
                 fontSize: 18.0,
                 color: Colors.grey[600],
@@ -58,7 +123,19 @@ class Profile extends StatelessWidget {
                 color: Colors.grey[600],
               ),
             ),
-            const SizedBox(height: 10.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBioCard() {
+    return Card(
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
             const Text(
               'Bio:',
               style: TextStyle(
@@ -66,67 +143,17 @@ class Profile extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 5.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in massa neque. Nulla commodo nulla quis facilisis bibendum. Morbi eget diam vitae ex rhoncus tincidunt. Mauris posuere risus vel enim dapibus, nec sollicitudin metus elementum. Duis sit amet est nec velit feugiat volutpat. Sed in sodales odio.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-            ),
             const SizedBox(height: 10.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const EditProfilePage()),
-                    );
-                  },
-                  child: const Text('Edit Profile'),
-                ),
-                const SizedBox(width: 10.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: const Text('Logout'),
-                ),
-              ],
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in massa neque. Nulla commodo nulla quis facilisis bibendum. Morbi eget diam vitae ex rhoncus tincidunt. Mauris posuere risus vel enim dapibus, nec sollicitudin metus elementum. Duis sit amet est nec velit feugiat volutpat. Sed in sodales odio.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.grey[600],
+              ),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  //make me user profile page ui
-
-  AppBar _appBar() {
-    return AppBar(
-      centerTitle: true,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('lib/assets/images/logo.png',
-                fit: BoxFit.contain, height: 32),
-          ),
-          const Text('PetYatu',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
-              )),
-        ],
       ),
     );
   }
@@ -136,7 +163,6 @@ class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _EditProfilePageState createState() => _EditProfilePageState();
 }
 
@@ -168,7 +194,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
         title: const Text('Edit Profile'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -220,7 +246,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 10.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
                 // Update user profile with new information
