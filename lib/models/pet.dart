95ab3f0d0460fa6ac565ad37f0f9dd88f498ad01
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2023-05-29 15:20:01 +0800

    Updated pet model

--- lib/models/pet.dart
+++ lib/models/pet.dart
@@ -2,15 +2,40 @@
 class Pet {
   String uid;
   String name;
-  String type;
-  String image;
+  int age;
+  String? type;
+  String? image;
+  List<String>? gallery;
+  String owner;
+  String location;
+  DateTime? datePosted;
+  String bio;
 
   Pet({
     required this.uid,
     required this.name,
     required this.type,
     required this.image,
+    required this.age,
+    required this.gallery,
+    required this.owner,
+    required this.location,
+    required this.datePosted,
+    required this.bio,
   });
+
+  // empty constructor
+  Pet.empty()
+      : uid = '',
+        name = '',
+        type = '',
+        image = '',
+        age = 0,
+        gallery = [],
+        owner = '',
+        location = '',
+        datePosted = null,
+        bio = '';
 
   factory Pet.fromJson(Map<String, dynamic> json) {
     return Pet(
@@ -18,6 +43,12 @@ class Pet {
       name: json['name'],
       type: json['type'],
       image: json['image'],
+      age: json['age'],
+      gallery: json['gallery'],
+      owner: json['owner'],
+      location: json['location'],
+      datePosted: json['datePosted'].toDate(),
+      bio: json['bio'],
     );
   }
 
